caedcb0c3c9637633c4328e771ee6f11e5dddab0
Michael Rallo <msr5zb@mail.missouri.edu>
2016-09-19 23:18:03 -0500

    Renames2

--- php/contact.php
+++ php/contact.php
@@ -1,8 +1,8 @@
 <?php
 
 // configure
-$from = 'Demo contact form <demo@domain.com>'; 
-$sendTo = 'Testing This <rallomikework@gmail.com>';
+$from = 'A Customer on Your WebSite!'; 
+$sendTo = 'rallomikework@gmail.com';
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in email
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';

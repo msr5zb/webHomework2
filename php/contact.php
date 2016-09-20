9fdd8e4782557431ba12a620837728f3b26773af
Michael Rallo <msr5zb@mail.missouri.edu>
2016-09-19 23:28:48 -0500

    More Forms

--- php/contact.php
+++ php/contact.php
@@ -1,8 +1,8 @@
 <?php
 
 // configure
-$from = 'A Customer on Your WebSite!'; 
-$sendTo = 'rallomikework@gmail.com';
+$from = '<rallomike@yahoo.com>'; 
+$sendTo = '<rallomikework@gmail.com>';
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in email
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
